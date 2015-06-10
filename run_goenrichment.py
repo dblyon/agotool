9573fbe77db82fc191e4d3a72895c55a506dc79e
David Lyon <dblyon@gmail.com>
2015-06-10 14:39:05 +0200

    temp commit
    Former-commit-id: 9e0ed24f3a75683b6a2b1c93bfbca43413afd940 [formerly 9e0ed24f3a75683b6a2b1c93bfbca43413afd940 [formerly 9e0ed24f3a75683b6a2b1c93bfbca43413afd940 [formerly a73bdb4fea17cf693ad50fbaf5a8345ca35d335a]]]
    Former-commit-id: 28ec2368d424928940982252236dabac414c820a
    Former-commit-id: d2cbce477d7f3a934ea99fb5b77c315349ab918c
    Former-commit-id: 4407368588b4aa2e131ab43e4d6dbf10b9e995ba

--- run_goenrichment.py
+++ run_goenrichment.py
@@ -17,11 +17,12 @@ if __name__ == '__main__':
     obo_dag = obo_parser.GODag(obo_file=obo_fn)
     assoc_dict = goretriever.Parser_UniProt_goa_ref(goa_ref_fn = goa_ref_fn).get_association_dict()
 
-    userinput_fn = r'/Users/dblyon/CloudStation/CPR/Brian_GO/UserInput.txt'
+    # userinput_fn = r'/Users/dblyon/CloudStation/CPR/Brian_GO/UserInput.txt'
+    # userinput_fn = r'/Users/dblyon/CloudStation/CPR/Brian_GO/alldata/Yest_Acetyl_vs_Genome_UserInput.txt'
     # userinput_fn = home + r'/modules/cpr/goterm/test/UserInput_test2.txt'
     home = os.path.expanduser('~')
 
-    randomSample = True
+
     backtracking = True
     num_bins = 100
     alpha = 0.05
@@ -56,11 +57,48 @@ if __name__ == '__main__':
     # for i in range(1,11):
     #     fn_out = 'SummaryTest_yeast_acetyl_randomSample_v' + str(i) + '.txt'
 
-    fn_out = 'SummaryTest_yeast_acetyl_AbundanceCorrected_btrk_rndom1.txt'
-    ui = goretriever.UserInput(userinput_fn, num_bins) # adapt UserInput to process web input etc.
-    gostudy = go_enrichment_dbl.GOEnrichmentStudy(ui, assoc_dict, obo_dag, alpha, methods, backtracking, randomSample)
-    results = gostudy.run_study()
-    gostudy.write_summary2file(fn_out, min_ratio=min_ratio, indent=indent, pval=pval)
+    # fn_out = 'Yeast_Acetyl_vs_Genome.txt'
+
+
+
+    # fn_out = 'TEST_v7.txt'
+    # col_sample_an = 'sample_an'
+    # col_background_an = 'backgrnd_an'
+    # col_background_int = 'backgrnd_int'
+
+# YEAST
+# ['Genome',
+#  'iBAQ observed (log10)',
+#  'Observed Proteome',
+#  'Phos',
+#  'Ubi',
+#  'Acetyl',
+#  'Succinyl']
+
+# 1- modified v genome
+# 2- modified v observed
+# 3- modified v Abundance-corrected
+
+
+    randomSample = False
+    userinput_fn = r'/Users/dblyon/CloudStation/CPR/Brian_GO/alldata/Data_for_web_tool_Yeast.txt'
+
+    for modification in ['Phos', 'Ubi', 'Acetyl', 'Succinyl']:
+        for background in ['Genome', 'Observed', 'AbCorr']:
+            fn_out = 'Yest_modification_vs_background.txt'.replace('modification', modification)
+            fn_out = 'Yest_modification_vs_background.txt'.replace('background', background)
+            if background == 'Genome':
+                abcorr = False
+            else:
+                abcorr = True
+            col_sample_an = modification
+            col_background_an = 'Genome'
+            col_background_int = 'iBAQ observed (log10)'
+
+            ui = goretriever.UserInput(userinput_fn, num_bins, col_sample_an, col_background_an, col_background_int)
+
+            gostudy = go_enrichment_dbl.GOEnrichmentStudy(ui, assoc_dict, obo_dag, alpha, methods, backtracking, randomSample, abcorr)
+            gostudy.write_summary2file(fn_out, min_ratio=min_ratio, indent=indent, pval=pval)
 
 
 
@@ -77,6 +115,17 @@ if __name__ == '__main__':
     # study_fn = r'/Users/dblyon/CloudStation/CPR/Brian_GO/go_rescources/input_goatools/study_yeast_acetyl.txt'
     # population_fn = r'/Users/dblyon/CloudStation/CPR/Brian_GO/go_rescources/input_goatools/population_yeast_obsProteome.txt'
     # association_fn = r'/Users/dblyon/CloudStation/CPR/Brian_GO/go_rescources/input_goatools/association_goa_yeast'
+
+
+
+
+
+
+
+
+
+
+
 
 
 
