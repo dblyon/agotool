0b5d1c046f887b4676b544a3cdca09d430d29da5
David Lyon <dblyon@gmail.com>
2019-04-11 16:53:17 +0200

    temp

--- app/python/variables.py
+++ app/python/variables.py
@@ -205,16 +205,22 @@ blacklisted_terms = {'GO:0003674', 'GO:0005575', 'GO:0008150',
                      'KW-9990', 'KW-9991', 'KW-9992', 'KW-9993', 'KW-9994', 'KW-9997', 'KW-9998', 'KW-9999'}
 
 ##### final Tables / flat-files needed for flask app / PostgreSQL
-tables_dict = {
-    "Entity_types_table": os.path.join(TABLES_DIR, "Entity_types_table_FIN.txt"),
-    "Functions_table": os.path.join(TABLES_DIR, "Functions_table_FIN.txt"),
-    "KEGG_Taxid_2_acronym_table": os.path.join(TABLES_DIR, "KEGG_Taxid_2_acronym_table_FIN.txt"),
-    "Lineage_table": os.path.join(TABLES_DIR, "Lineage_table_FIN.txt"),
-    "Protein_2_FunctionEnum_and_Score_table": os.path.join(TABLES_DIR, "Protein_2_FunctionEnum_and_Score_table_FIN.txt"),
-    "Protein_2_FunctionEnum_table": os.path.join(TABLES_DIR, "Protein_2_FunctionEnum_table_FIN.txt"),
-    "Taxid_2_FunctionCountArray_table": os.path.join(TABLES_DIR, "Taxid_2_FunctionCountArray_table_FIN.txt"),
-    "Taxid_2_Proteins_table": os.path.join(TABLES_DIR, "Taxid_2_Proteins_table_FIN.txt"),
-}
+# tables_dict = {
+#     "Entity_types_table": os.path.join(TABLES_DIR, "Entity_types_table_FIN.txt"),
+#     "Functions_table": os.path.join(TABLES_DIR, "Functions_table_FIN.txt"),
+#     "KEGG_Taxid_2_acronym_table": os.path.join(TABLES_DIR, "KEGG_Taxid_2_acronym_table_FIN.txt"),
+#     "Lineage_table": os.path.join(TABLES_DIR, "Lineage_table_FIN.txt"),
+#     "Protein_2_FunctionEnum_and_Score_table": os.path.join(TABLES_DIR, "Protein_2_FunctionEnum_and_Score_table_FIN.txt"),
+#     "Protein_2_FunctionEnum_table": os.path.join(TABLES_DIR, "Protein_2_FunctionEnum_table_FIN.txt"),
+#     "Taxid_2_FunctionCountArray_table": os.path.join(TABLES_DIR, "Taxid_2_FunctionCountArray_table_FIN.txt"),
+#     "Taxid_2_Proteins_table": os.path.join(TABLES_DIR, "Taxid_2_Proteins_table_FIN.txt"),
+# }
+
+blacklisted_terms = {'GO:0003674', 'GO:0005575', 'GO:0008150',
+                     'KW-0002', 'KW-0181', 'KW-0308', 'KW-0374', 'KW-0582', 'KW-0614',
+                     'KW-0814', 'KW-0895', 'KW-0903', 'KW-0952', 'KW-1185', 'KW-1267',
+                     'KW-9990', 'KW-9991', 'KW-9992', 'KW-9993', 'KW-9994', 'KW-9997', 'KW-9998', 'KW-9999'}
+# 'KW-9990' 'Technical term' and all its children
 
 def get_blacklisted_enum_terms(fn_functions_table, blacklisted_terms):
     "| enum | etype | an | description | year | level |"
@@ -229,7 +235,6 @@ def get_blacklisted_enum_terms(fn_functions_table, blacklisted_terms):
     blacklisted_enum_terms = sorted(blacklisted_enum_terms)
     return np.array(blacklisted_enum_terms, dtype=np.dtype("uint32"))
 
-fn_functions_table = tables_dict["Functions_table"]
+fn_functions_table = os.path.join(TABLES_DIR, "Functions_table_STRING.txt")
 blacklisted_enum_terms = get_blacklisted_enum_terms(fn_functions_table, blacklisted_terms)
 
-jensenlab_score_cutoff_list = [4.0, 3.0, 2.0, 1.0, 0.0]
