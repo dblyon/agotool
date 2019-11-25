eeb617597be55c675909008ef10c996698b3936c
David Lyon <dblyon@gmail.com>
2019-11-25 17:18:22 +0100

    sync to perform randomization tests for KS

--- app/c/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ app/c/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -47,5 +47,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/c.dir/DependInfo.cmake"
+  "CMakeFiles/compute_global_enrichment_DBL.dir/DependInfo.cmake"
   )
---
+++ app/c/cmake-build-debug/CMakeFiles/compute_global_enrichment_DBL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../compute_global_enrichment_DBL"
+  "../compute_global_enrichment_DBL.pdb"
+  "CMakeFiles/compute_global_enrichment_DBL.dir/compute_global_enrichment_DBL.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/compute_global_enrichment_DBL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
