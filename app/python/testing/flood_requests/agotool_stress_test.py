3837fc2eac81724b46c2234e8b2ae29ba2b5f2ff
David Lyon <dblyon@gmail.com>
2019-12-20 12:35:40 +0100

    wrapping up agotool_stress_test.py and pytest test_flood_requests.py

--- app/python/testing/flood_requests/agotool_stress_test.py
+++ app/python/testing/flood_requests/agotool_stress_test.py
@@ -1,6 +1,5 @@
 import sys, os, time, argparse, subprocess, datetime
 
-
 def error_(parser):
     sys.stderr.write("The arguments passed are invalid.\nPlease check the input parameters.\n\n")
     parser.print_help()
@@ -8,12 +7,24 @@ def error_(parser):
 
 argparse_parser = argparse.ArgumentParser()
 argparse_parser.add_argument("IP", help="IP address without port, e.g. '127.0.0.1' (is also the default)", type=str, default="0.0.0.0", nargs="?")
-argparse_parser.add_argument("--port", help="port number, e.g. '10110' (is also the default)", type=str, default="10110", nargs="?")
+argparse_parser.add_argument("--port", help="port number, e.g. '10110' (is also the default)", type=str, default="10111", nargs="?")
 argparse_parser.add_argument("prefix", help="prefix of directory to store results, e.g. 'test_v1' ", type=str, default="test_agotool_v8", nargs="?")
 argparse_parser.add_argument("parallel_processes", help="number of parallel processes for flooding, e.g. 50", type=int, default=50, nargs="?")
 argparse_parser.add_argument("parallel_iterations", help="total total number of iterations for parallel test, e.g. 1000 (parallel_processes: number of synchronous requests, parallel_iterations: total num requests) ", type=int, default=50000, nargs="?")
 argparse_parser.add_argument("sequential_iterations", help="total number of iterations (for 2 parallel but otherwise) sequential requests, e.g. 10000 (2 parallel requests * 1000 = 2000).", type=int, default=10000, nargs="?")
 argparse_parser.add_argument("verbose", help="be verbose or not. print things.", type=bool, default=True, nargs="?")
+# """
+# example of files being created (in directory 'test_agotool_v8') when running this script:
+# 'log_requests.txt',
+# 'log_settings_and_analysis.txt',
+# 'log_uwsgi_requests.txt',
+# 'parallel_test_agotool_v8_0.txt',
+# 'parallel_test_agotool_v8_1.txt',
+# 'parallel_test_agotool_v8_3.txt',
+# 'sequential_test_agotool_v8_WRONG_9994_0.txt',
+# 'sequential_test_agotool_v8_WRONG_9995_2.txt',
+# 'sequential_test_agotool_v8_HUMAN_10000_3.txt'
+# """
 
 args = argparse_parser.parse_args()
 for arg in sorted(vars(args)):
@@ -76,8 +87,24 @@ with open(log_fn_settings, "a") as fh_log:
     flood.wait()
     flood2.wait()
 
+def rsync_uWSGI_log_files():
+    """
+    log_uwsgi_requests.txt and log_uwsgi_error.txt
+    try copying uWSGI log files to prefix directory. The log files are located in the same directory the uwsgi app was launched in (using "uwsgi uwsgi_config.ini")
+    # --> ./agotool/app
+    """
+    cmd = "rsync -av --update ../../../log_uwsgi_\* ./{}".format(prefix)
+    print("Copying uWSGI log files to prefix directory")
+    print(cmd)
+    rsync = subprocess.Popen(cmd, shell=True)
+    rsync.wait()
+
+rsync_uWSGI_log_files()
+
 ### Test to do
 # count the number of parallel and sequential files
 # cut -f colname with p_values cat all files that are not non-sense | sort | uniq -c | check if anything less than minimum amount of requests
 # plot requests delta over time / over number of requests
 # facet plot by parallel requests vs sequential requests
+
+# rename files in backup for STRING v11
--- app/python/testing/flood_requests/test_flood_requests.py
+++ app/python/testing/flood_requests/test_flood_requests.py
@@ -2,19 +2,16 @@ import sys, os
 import pytest
 import pandas as pd
 import numpy as np
+import subprocess, multiprocessing
+import matplotlib.pyplot as plt
+
 ### go 2 levels up and add to path
 python_modules_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(os.path.realpath(__file__))), "../../"))
 sys.path.insert(0, python_modules_dir)
 
-
-
-
 import variables
 # TEST_FN_DIR = os.path.join(os.path.dirname(os.path.abspath(os.path.realpath(__file__))), "test")
 PYTEST_FN_DIR = variables.PYTEST_FN_DIR
-
-
-
 
 ### parse directory of uWSGI. 2 log files from uWSGI and 2 log files from DBL code
 # log_requests.txt  log_settings_and_analysis.txt  log_uwsgi_error.txt  log_uwsgi_requests.txt
@@ -22,8 +19,15 @@ PYTEST_FN_DIR = variables.PYTEST_FN_DIR
 @pytest.fixture(scope='module')
 def test_dir():
     # dir_ = r"/Users/dblyon/modules/cpr/agotool/app/python/load_test/test_2_workers"
-    dir_ = r"/Users/dblyon/modules/cpr/agotool/app/python/testing/flood_requests/test_agotool_v8"
+    dir_ = r"/Users/dblyon/modules/cpr/agotool/app/python/testing/flood_requests/test_agotool_v9"
+    print("#" * 50)
+    print("TESTING: ", dir_)
+    print("#" * 50)
     return dir_
+
+@pytest.fixture(scope='module')
+def get_prefix():
+    return "test_agotool_v8"
 
 @pytest.fixture(scope='module')
 def get_fn_log_DBL_requests():
@@ -35,6 +39,48 @@ def get_fn_log_uWSGI_requests():
     fn_log_uWSGI_requests = "log_uwsgi_requests.txt"
     return fn_log_uWSGI_requests
 
+@pytest.fixture(scope='module')
+def get_number_of_processes_for_sort():
+    NUMBER_OF_PROCESSES = multiprocessing.cpu_count()
+    if NUMBER_OF_PROCESSES > 10:
+        NUMBER_OF_PROCESSES_sorting = 10
+    else:
+        NUMBER_OF_PROCESSES_sorting = NUMBER_OF_PROCESSES
+    return NUMBER_OF_PROCESSES_sorting
+
+@pytest.fixture(scope='module')
+def get_parallel_request_files(test_dir):
+    """
+    human foreground and background --> results should all be the same and deterministic
+    """
+    parallel_file_names = []
+    for fn in os.listdir(test_dir):
+        if fn.startswith("parallel_"):
+            parallel_file_names.append(fn)
+    return parallel_file_names
+
+@pytest.fixture(scope='module')
+def get_sequential_HUMAN_request_files(test_dir):
+    """
+    same as parallel but first column contains prefix in first column
+    human foreground and background --> results should all be the same and deterministic
+    """
+    sequential_HUMAN_file_names = []
+    for fn in os.listdir(test_dir):
+        if fn.startswith("sequential_") and "_HUMAN_" in fn:
+            sequential_HUMAN_file_names.append(fn)
+    return sequential_HUMAN_file_names
+
+@pytest.fixture(scope='module')
+def get_sequential_WRONG_request_files(test_dir):
+    """
+    random taxon as background
+    """
+    sequential_WRONG_file_names = []
+    for fn in os.listdir(test_dir):
+        if fn.startswith("sequential_") and "_WRONG_" in fn:
+            sequential_WRONG_file_names.append(fn)
+    return sequential_WRONG_file_names
 
 @pytest.fixture(scope='module')
 def dfu(test_dir, get_fn_log_uWSGI_requests):
@@ -69,6 +115,50 @@ def dfu(test_dir, get_fn_log_uWSGI_requests):
     dfu["code"] = html_response_code_l
     dfu["time_of_request"] = pd.to_datetime(time_of_request_l)
     return dfu
+
+def test_plot_uWSGI_TimeOfRequest_vs_ResponseTime(dfu, test_dir):
+    """
+    things to check out if problems arrise
+     - total requests per minute: dfu.groupby(pd.Grouper(key='time_of_request', freq='min')).count().reset_index().plot(x="time_of_request", y="code")
+    dfu.boxplot(column=["response_time"], by="PID")
+
+    dfu.plot(x="time_of_request", y="response_time")
+    # plt.savefig('DFU_TimeOfRequest_vs_ResponseTime.png')
+    dfu["time_numeric"] = pd.to_numeric(dfu["time_of_request"])
+    dfu.plot(x="time_numeric", y="response_time", kind="scatter")
+    dfu.sort_values("response_time", ascending=False).iloc[1:].plot(x="time_of_request", y="response_time")
+    # plt.savefig('DFU_TimeOfRequest_vs_ResponseTime_zoom.png')
+    """
+    fn_1 = os.path.join(test_dir, 'DFU_TimeOfRequest_vs_ResponseTime.png')
+    dfu.plot(x="time_of_request", y="response_time")
+    plt.savefig(fn_1)
+    plt.close()
+
+    fn_1_v2 = os.path.join(test_dir, 'DFU_TimeOfRequest_vs_ResponseTime_v2.png')
+    dfu["time_numeric"] = pd.to_numeric(dfu["time_of_request"])
+    dfu.plot(x="time_numeric", y="response_time", kind="scatter")
+    plt.savefig(fn_1_v2)
+    plt.close()
+
+    fn_2 = os.path.join(test_dir, 'DFU_TimeOfRequest_vs_ResponseTime_zoom.png')
+    dfu.sort_values("response_time", ascending=False).iloc[10:].plot(x="time_of_request", y="response_time")
+    plt.savefig(fn_2)
+    plt.close()
+
+    # plot number of requests per minute per PID # plot fails with big log file, no time to figure out why at the moment
+    # fn_3 = os.path.join(test_dir, 'DFU_NumberRequestsPerMinutePerPID.png')
+    # x = dfu.groupby(pd.Grouper(key='time_of_request', freq='min'))['PID'].value_counts().reset_index(name="count")
+    # for key, group in x.groupby(["PID"]):
+    #     ax = plt.plot(group["count"], label=key)
+    # plt.legend(loc='best')
+    # plt.savefig(fn_3)
+    # plt.close()
+
+    assert os.path.isfile(fn_1)
+    assert os.path.isfile(fn_1_v2)
+    assert os.path.isfile(fn_2)
+    # assert os.path.isfile(fn_3)
+
 
 @pytest.fixture(scope='module')
 def dfr(test_dir, get_fn_log_DBL_requests):
@@ -107,6 +197,8 @@ def dfr(test_dir, get_fn_log_DBL_requests):
     dfr["delta_millisec"] = (dfr["timestamp"].iloc[1:].reset_index(drop=True) - dfr["timestamp"].iloc[:-1].reset_index(drop=True)).apply(lambda x: x.delta / 1e6)
     return dfr
 
+
+
 def test_if_warnings(test_dir, get_fn_log_DBL_requests):
     """
     were any 'WARNING!' generated by parallel_requests.py or sequential_requests.py
@@ -131,8 +223,51 @@ def test_long_response_time(dfu):
     maximum_response_time_in_milli_seconds = dfu["response_time"].max()
     assert maximum_response_time_in_milli_seconds <= 1000
 
-def test_deterministic_results():
+def test_results_parallel(test_dir, get_prefix, get_parallel_request_files, get_number_of_processes_for_sort):
     """
-    results should always yield the same results
+    are results deterministic
+    results should always yield the exact same results --> unique count of p_values (3rd column not the same for HUMAN) should be equal to number of files
+    """
+    # check that entire line is the same in each file
+    output_fn = os.path.join(test_dir, "temp_test_deterministic_results_parallel.txt")
+    cmd = r'find {} -type f -name "parallel_{}_*" | xargs cut -f 3 | LC_ALL=C sort --parallel {} | uniq -c > {}'.format(test_dir, get_prefix, get_number_of_processes_for_sort, output_fn)
+    check_parallel_results = subprocess.Popen(cmd, shell=True)
+    check_parallel_results.wait()
+    number_of_files = len(get_parallel_request_files)
+    with open(output_fn, "r") as fh:
+        for line in fh:
+            number_of_rows_with_this_content, content = line.split()
+            assert int(number_of_rows_with_this_content) >= number_of_files
 
+def test_results_deterministic_sequential_HUMAN_request(test_dir, get_prefix, get_sequential_HUMAN_request_files, get_number_of_processes_for_sort):
     """
+    are results deterministic
+    results should always yield the exact same results --> unique count of p_values (4th column) should be equal to number of files
+    """
+    # check that p_values are the same in all file
+    output_fn = os.path.join(test_dir, "temp_test_deterministic_results_sequential_HUMAN.txt")
+    cmd = r'find {} -type f -name "parallel_{}_*" | xargs cut -f 4 | LC_ALL=C sort --parallel {} | uniq -c > {}'.format(test_dir, get_prefix, get_number_of_processes_for_sort, output_fn)
+    check_sequential_HUMAN_results = subprocess.Popen(cmd, shell=True)
+    check_sequential_HUMAN_results.wait()
+    number_of_files = len(get_sequential_HUMAN_request_files)
+    with open(output_fn, "r") as fh:
+        for line in fh:
+            number_of_rows_with_this_content, content = line.split()
+            assert int(number_of_rows_with_this_content) >= number_of_files
+
+def test_results_deterministic_sequential_WRONG_request(test_dir, get_prefix, get_sequential_WRONG_request_files, get_number_of_processes_for_sort):
+    """
+    are results deterministic
+    results should always yield the exact same results --> unique count of foreground_count (3rd column) should be at least as large as the number of files
+    """
+    # check that p_values are the same in all files
+    output_fn = os.path.join(test_dir, "temp_test_deterministic_results_sequential_WRONG.txt")
+    cmd = r'find {} -type f -name "parallel_{}_*" | xargs cut -f 3 | LC_ALL=C sort --parallel {} | uniq -c > {}'.format(test_dir, get_prefix, get_number_of_processes_for_sort, output_fn)
+    # LC_ALL=C sort --parallel {}
+    check_sequential_WRONG_results = subprocess.Popen(cmd, shell=True)
+    check_sequential_WRONG_results.wait()
+    number_of_files = len(get_sequential_WRONG_request_files)
+    with open(output_fn, "r") as fh:
+        for line in fh:
+            number_of_rows_with_this_content, content = line.split()
+            assert int(number_of_rows_with_this_content) >= number_of_files
