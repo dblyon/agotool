1fb1d51da85064bfedc22d21f6a236be7fc669b9
David Lyon <dblyon@gmail.com>
2020-05-13 21:15:58 +0200

    fixed Json response

--- app/runserver.py
+++ app/runserver.py
@@ -341,7 +341,9 @@ def format_multiple_results(args_dict, results_all_entity_types, pqo):
     elif output_format == "json":
         # return jsonify(results_all_entity_types)
         # return results_all_entity_types.to_json(orient="records")
-        return results_all_entity_types
+        # return results_all_entity_types
+        return app.response_class(response=results_all_entity_types, status=200, mimetype='application/json')
+
     elif output_format == "xml":
         # dict_2_return = {}
         # for etype, results in results_all_entity_types.items():
