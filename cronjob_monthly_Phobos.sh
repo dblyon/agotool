592a5ce89da2f2ff86a7b7744c70cd95285cffe4
David Lyon <dblyon@gmail.com>
2020-09-21 13:39:29 +0000

    cronjob paths

--- cronjob_monthly_Phobos.sh
+++ cronjob_monthly_Phobos.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 # shellcheck disable=SC2038
 # shellcheck disable=SC2164
 # shellcheck disable=SC2028
 # shellcheck disable=SC2181
-
 check_exit_status () {
   if [ ! $? = 0 ]; then exit; fi
 }
@@ -16,25 +14,25 @@ TAR_BAK=bak_aGOtool_flatfiles_current$(date +"%Y_%m_%d_%I_%M_%p").tar.bz2
 echo "--- Cronjob starting "$(date +"%Y_%m_%d_%I_%M_%p")" ---"
 
 #### tar and compress previous files for backup --> commented out: since too many backups
-#echo "\n### tar and compress previous files for backup\n"
-#TAR_FILE_NAME=bak_aGOtool_flatfiles_$(date +"%Y_%m_%d_%I_%M_%p").tar
-#cd /mnt/mnemo5/dblyon/agotool/data/PostgreSQL/tables
-##### create tar of relevant flat files
-#find . -maxdepth 1 -name '*.npy' -o -name '*_UPS_FIN*' | xargs tar cvf $TAR_FILE_NAME
-#check_exit_status
-##### compress for quick transfer and backup, this can run in the background since it's independent of snakemake
-#pbzip2 -p10 $TAR_FILE_NAME &
-#check_exit_status
+echo "\n### tar and compress previous files for backup\n"
+TAR_FILE_NAME=bak_aGOtool_flatfiles_$(date +"%Y_%m_%d_%I_%M_%p").tar
+cd /scratch/dblyon/agotool/data/PostgreSQL/tables
+#### create tar of relevant flat files
+find . -maxdepth 1 -name '*.npy' -o -name '*_UPS_FIN*' | xargs tar cvf $TAR_FILE_NAME
+check_exit_status
+#### compress for quick transfer and backup, this can run in the background since it's independent of snakemake
+pbzip2 -p10 $TAR_FILE_NAME &
+check_exit_status
 
 ### run snakemake pipeline
 echo "\n### run snakemake pipeline\n"
 cd /scratch/dblyon/agotool/app/python
-/mnt/mnemo5/dblyon/install/anaconda3/envs/agotoolv2/bin/snakemake -l | tr '\n' ' ' | xargs /mnt/mnemo5/dblyon/install/anaconda3/envs/agotoolv2/bin/snakemake -j 10 -F
+/mnt/mnemo4/dblyon/install/anaconda3/envs/agotoolv2/bin/snakemake -l | tr '\n' ' ' | xargs /mnt/mnemo4/dblyon/install/anaconda3/envs/agotoolv2/bin/snakemake -j 10 -F
 check_exit_status
 
 # add file dimensions to log for testing and debugging
 cd /scratch/dblyon/agotool/app/python
-/mnt/mnemo5/dblyon/install/anaconda3/envs/agotoolv2/bin/python -c 'import create_SQL_tables_snakemake; create_SQL_tables_snakemake.add_2_DF_file_dimensions_log()'
+/mnt/mnemo4/dblyon/install/anaconda3/envs/agotoolv2/bin/python -c 'import create_SQL_tables_snakemake; create_SQL_tables_snakemake.add_2_DF_file_dimensions_log()'
 
 # automated testing here!!! ToDo if tests pass --> then proceed with the rest
 
@@ -52,7 +50,7 @@ check_exit_status
 
 # copy files to Aquarius (production server)
 echo "\n### copy files to Aquarius (production server)\n"
-rsync -av /mnt/mnemo5/dblyon/agotool/data/PostgreSQL/tables/"$TAR_CURRENT".bz2 dblyon@aquarius.meringlab.org:/home/dblyon/agotool/data/PostgreSQL/tables/"$TAR_CURRENT".bz2
+rsync -av /scratch/dblyon/agotool/data/PostgreSQL/tables/"$TAR_CURRENT".bz2 dblyon@aquarius.meringlab.org:/home/dblyon/agotool/data/PostgreSQL/tables/"$TAR_CURRENT".bz2
 check_exit_status
 
 # on production server, decompress files, populate DB, restart service
@@ -87,8 +85,5 @@ echo "\n--- finished Cronjob ---\n"
 ### dblyon inserted cronjob for automated aGOtool updates
 ## testing 10:05 am
 # 1 1 1 * * /mnt/mnemo5/dblyon/agotool/cronjob_monthly_Phobos.sh >> /mnt/mnemo5/dblyon/agotool/log_cron_monthly_snakemake.txt 2>&1
-<<<<<<< HEAD:cronjob_monthly_Phobos.sh
 ########################################################################################################################
-=======
 ########################################################################################################################
->>>>>>> cfd391b1cf56f8ac0056e1ed0401b263fee36aa8:cronjob_monthly_Atlas.sh
