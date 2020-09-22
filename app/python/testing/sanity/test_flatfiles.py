89e5e6f36f810862c284f50c52ec716c0f594dff
David Lyon <dblyon@gmail.com>
2020-09-22 14:25:46 +0000

    fixed Pytests for DF_file_dimensions_log

--- app/python/testing/sanity/test_flatfiles.py
+++ app/python/testing/sanity/test_flatfiles.py
@@ -6,6 +6,14 @@ import numpy as np
 
 import variables
 
+### adding files to deprecated list
+# df["deprecated"] = False
+# cond = df["fn"].isin(["Taxid_2_FunctionEnum_2_Scores_table_UPS_FIN.txt", "Taxid_2_FunctionEnum_2_Scores_dict_UPS_FIN.p",
+#                       "Protein_2_FunctionEnum_and_Score_table_UPS_FIN.txt", "rowIndex_2_ENSP_dict_UPS_FIN.p",
+#                       "CSC_ENSPencoding_2_FuncEnum_UPS_FIN.npz", "ENSP_2_rowIndex_dict_UPS_FIN.p",
+#                      "SparseMatrixCSC_ENSPencoding_vs_FuncEnum_UPS_FIN.npz"])
+# df.loc[cond, "deprecated"] = True
+# df.to_csv(LOG_DF_FILE_DIMENSIONS, sep="\t", header=True, index=False)
 
 
 # check if files are similar size of larger than previously
@@ -14,6 +22,7 @@ import variables
 # read old table and add data to it
 LOG_DF_FILE_DIMENSIONS = variables.LOG_DF_FILE_DIMENSIONS
 df = pd.read_csv(LOG_DF_FILE_DIMENSIONS, sep="\t")
+df = df[df["deprecated"] == False] # skip TextMining Scores tables
 version_current = max(df["version"])
 version_previous = version_current - 1
 cond_previous = df["version"] == version_previous
@@ -25,14 +34,28 @@ def test_flatfiles_number_of_lines_similar_or_larger():
     # for every file, compare previous vs current number of lines
     for fn in sorted(df.loc[cond_not_binary, "fn"].unique()):
         cond_fn = df["fn"] == fn
-        num_lines_previous = df.loc[cond_previous & cond_fn, "num_lines"].values[0]
-        num_lines_current = df.loc[cond_current & cond_fn, "num_lines"].values[0]
+        cond_fnp = cond_previous & cond_fn
+        cond_fnc = cond_current & cond_fn
+        if sum(cond_fnp) == 0:
+            print("{} not present in previous version".format(fn))
+        if sum(cond_fnc) == 0:
+            print("{} not present in current version".format(fn))
+        num_lines_previous = df.loc[cond_fnp, "num_lines"].values[0]
+        num_lines_current = df.loc[cond_fnc, "num_lines"].values[0]
         assert num_lines_current >= num_lines_previous
 
 def test_compare_file_size():
     # for every file, compare previous vs current size of file
     for fn in sorted(df["fn"].unique()):
         cond_fn = df["fn"] == fn
-        size_previous = df.loc[cond_previous & cond_fn, "size"].values[0]
-        size_current  = df.loc[cond_current & cond_fn, "size"].values[0]
+        cond_fnp = cond_previous & cond_fn
+        cond_fnc = cond_current & cond_fn
+        if sum(cond_fnp) == 0:
+            print("{} not present in previous version".format(fn))
+        if sum(cond_fnc) == 0:
+            print("{} not present in current version".format(fn))
+        size_previous = df.loc[cond_fnp, "size"].values[0]
+        size_current = df.loc[cond_fnc, "size"].values[0]
         assert size_current >= size_previous
+
+        
