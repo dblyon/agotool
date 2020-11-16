41d99dd6e3b42c0f0a11cb8c8615050179611828
David Lyon <dblyon@gmail.com>
2020-11-16 12:42:14 +0100

    cd || exit in cronjobs

--- cronjob_update_aGOtool_Aquarius.sh
+++ cronjob_update_aGOtool_Aquarius.sh
@@ -11,7 +11,7 @@ check_exit_status () {
 }
 TABLES_DIR=/home/dblyon/agotool/data/PostgreSQL/tables
 PYTEST_EXE=/home/dblyon/anaconda3/envs/agotoolv2/bin/pytest
-UWSGI_EXE=/home/dblyon/anaconda3/envs/agotoolv2/bin/uwsgi
+# UWSGI_EXE=/home/dblyon/anaconda3/envs/agotoolv2/bin/uwsgi
 TESTING_DIR=/home/dblyon/agotool/app/python/testing/sanity
 APP_DIR=/home/dblyon/agotool/app
 POSTGRES_DIR=/home/dblyon/agotool/data/PostgreSQL
@@ -20,7 +20,7 @@ POSTGRES_DIR=/home/dblyon/agotool/data/PostgreSQL
 ### decompress files
 echo "running script on production server cronjob_update_aGOtool_Aquarius.sh @ "$(date +"%Y_%m_%d_%I_%M_%p")" ---"
 printf "\n### unpacking tar.gz files\n"
-cd "$TABLES_DIR"
+cd "$TABLES_DIR" || exit
 tar --overwrite -xvzf "$TABLES_DIR"/aGOtool_flatfiles_current.tar.gz
 check_exit_status
 
@@ -31,13 +31,13 @@ check_exit_status
 
 ### copy from file to PostgreSQL
 echo "\n### copying to PostgreSQL\n"
-cd "$POSTGRES_DIR"
+cd "$POSTGRES_DIR" || exit
 check_exit_status
 psql -d agotool -p 8001 -f copy_from_file_and_index.psql
 check_exit_status
 ### drop old tables and rename temp tables
 printf "\n### drop and rename PostgreSQL\n"
-cd "$POSTGRES_DIR"
+cd "$POSTGRES_DIR" || exit
 psql -d agotool -p 8001 -f drop_and_rename.psql
 check_exit_status
 psql -d agotool -p 8001 -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO agotool;"
@@ -45,10 +45,10 @@ check_exit_status
 
 ### restart uWSGI and PyTest
 printf "\n### chain reloading of uWSGI flaskapp, sleep 4min and PyTest\n"
-cd "$APP_DIR"
+cd "$APP_DIR" || exit
 echo c > agotool_master.fifo
 sleep 4m
-cd "$TESTING_DIR"
+cd "$TESTING_DIR" || exit
 "$PYTEST_EXE"
 check_exit_status
 
