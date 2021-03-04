538f944fafc05e2c4d2bc0ccc037fefdf3ccc507
David Lyon <dblyon@gmail.com>
2021-03-04 08:10:06 +0100

    sync

--- cronjob_monthly_Phobos_ago_UP.sh
+++ cronjob_monthly_Phobos_ago_UP.sh
@@ -44,10 +44,9 @@ printf "\n ### drop and rename PostgreSQL \n"
 psql -d agotool -f drop_and_rename.psql
 check_exit_status
 
-### restart uWSGI and PyTest
-printf "\n ### chain reloading of uWSGI flaskapp, sleep 4min and PyTest \n"
+### start uWSGI and PyTest (zerg needs to be running)
+printf "\n ### starting uWSGI flaskapp, sleep 4min and PyTest \n"
 cd "$APP_DIR" || exit
-# echo c > agotool_master.fifo
 "$UWSGI_EXE" pytest_agotool.ini &
 printf "sleeping for 4min \n"
 sleep 4m
@@ -60,17 +59,9 @@ check_exit_status
 ### clean up
 printf "shutting down aGOtool workers"
 cd "$APP_DIR" || exit
-echo q > pytest.fifo
+echo Q > pytest.fifo
 check_exit_status
 
-#### copy files to Aquarius (production server)
-#echo "\n### copy files to Aquarius (production server)\n"
-#rsync -av "$TABLES_DIR"/"$TAR_CURRENT" dblyon@aquarius.meringlab.org:/home/dblyon/agotool/data/PostgreSQL/tables/"$TAR_CURRENT"
-#check_exit_status
-#### on production server, decompress files, populate DB, restart service
-#echo "now attempting to run script on production server cronjob_monthly_Aquarius_ago_UP.sh @ "$(date +"%Y_%m_%d_%I_%M_%p")" ---"
-#ssh dblyon@aquarius.meringlab.org '/home/dblyon/agotool/cronjob_monthly_Aquarius_ago_UP.sh &> /home/dblyon/agotool/data/logs/log_updates.txt & disown'
-#check_exit_status
 ### copy files to Pisces (production server)
 echo "\n### copy files to Pisces (production server)\n"
 rsync -av "$TABLES_DIR"/"$TAR_CURRENT" dblyon@pisces.meringlab.org:/home/dblyon/agotool/data/PostgreSQL/tables/"$TAR_CURRENT"
